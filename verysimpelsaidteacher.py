653141efabfac22a9a3b5dde1cd880ece4dd8449
Foxytisy <m@treiman.is>
2025-09-19 09:18:19 +0300

    cat

--- verysimpelsaidteacher.py
+++ verysimpelsaidteacher.py
@@ -33,10 +33,14 @@ print() #bailenos con el tuca tuca tuca tuca el nazo
 
 
 
-
-
 num = int(input("Ievadi skaitli: "))
 if num > 10:
     print("Jā")
 else:
     print("Nē")
+
+teksts = input("Ievadi tekstu: ")
+if len(teksts) >= 5:
+    print(teksts[0]+teksts[2]+teksts[4])
+else:
+    print("Ievadi garāku tekstu smh")
