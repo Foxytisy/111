b72e148509efea1c6590ee03d8d426cf57a8b6ea
Foxytisy <m@treiman.is>
2025-09-19 09:07:47 +0300

    hiii

---
+++ verysimpelsaidteacher.py
@@ -0,0 +1,42 @@
+name = input("Ievadi savu vārdu: ") #bailenos con el tuca
+print("Sveiks, " + name + "!") #bailenos con el nazo
+print() #bailenos con el tuca tuca tuca tuca el nazo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+num = int(input("Ievadi skaitli: "))
+if num > 10:
+    print("Jā")
+else:
+    print("Nē")
