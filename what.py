70afe86c1a6deeb78cb7c2c31f557e7cb0e22ae1
Martins <m@treiman.s>
2025-11-01 20:14:17 +0200

    a

--- what.py
+++ what.py
@@ -1,10 +1,19 @@
-n = int(input("Input number to squareroot"))
+n = float(input("Input number to squareroot"))
 i=0
 
 while True:
-    if n != round(i*i):
+    if n > i*i:
         i += 0.000001
     else:
         break
 
 print(i, "is the squareroot of it")
+
+def sqrt(val: float):
+    root = val / 2
+    for _ in range(10):
+        root = ((((root * root)-val)/(- (2 * root)))+root)
+
+    return root
+
+print(sqrt(n))
