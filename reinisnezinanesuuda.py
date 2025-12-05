476d73bf9f677dc06be5f9c8c819376d0110164f
Foxytisy <m@treiman.is>
2025-12-05 09:09:13 +0200

    awdijawdijoawdoijawdoawdoijawd

--- reinisnezinanesuuda.py
+++ reinisnezinanesuuda.py
@@ -18,5 +18,30 @@ for iii in range (0,20):
 for keyifesnuwfesonuwfesonuwfeoni, valueefnuifenuiwfeuiwfeui in list(vardnica2.items()):
     if valueefnuifenuiwfeuiwfeui % 2 == 0:
         vardnica2.pop(keyifesnuwfesonuwfesonuwfeoni)
-#mim
+
 print(vardnica2)
+
+#i/ni darbs
+
+vardn = {
+    "V": 0,
+    "VI": 0,
+    "VII": 0,
+    "VIII": 0,
+    "X": 0,
+    "XI": 0,
+    "XII": 0
+}
+
+for key, sk in list(vardn.items()):
+    for i in key:
+        if i == "I":
+            vardn[key] += 1
+        elif i == "V":
+            vardn[key] += 5
+        elif i == "X":
+            vardn[key] += 10
+        else:
+            print("Neatpazists romiešu whatever")
+    
+print(vardn)
