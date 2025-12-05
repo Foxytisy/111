e3718b3dc446b38beb5124b3aad09d4eb26fd1da
Foxytisy <m@treiman.is>
2025-12-05 08:50:23 +0200

    ajj

---
+++ reinisnezinanesuuda.py
@@ -0,0 +1,22 @@
+import random
+
+vardnica = {}
+for iii in range (0,20):
+    vardnica[iii] = random.randint(1, 1000)
+    
+for i in range(len(vardnica)):
+    if i%2!=0:
+        vardnica.pop(i)
+print(vardnica)   
+     
+     
+     
+vardnica2 = {}
+for iii in range (0,20):
+    vardnica2[iii] = random.randint(1, 1000)   
+  
+for keyifesnuwfesonuwfesonuwfeoni, valueefnuifenuiwfeuiwfeui in list(vardnica2.items()):
+    if valueefnuifenuiwfeuiwfeui % 2 == 0:
+        vardnica2.pop(keyifesnuwfesonuwfesonuwfeoni)
+
+print(vardnica2)
